9ffe241d95c525831b1c6c7e0cad4db0231d057a
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-04-28 17:44:27 -0400

    update Selectize to 0.12.1

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-    name: "jeremy:selectize",
-    summary: "Selectize is a hybrid of a textbox and <select> element used for tagging, contact lists, and more.",
-    version: '0.12.1',
-    git: 'https://github.com/jshimko/meteor-selectize.git'
+  name: "jeremy:selectize",
+  summary: "Selectize is a hybrid of a textbox and <select> element used for tagging, contact lists, and more.",
+  version: '0.12.1_2',
+  git: 'https://github.com/jshimko/meteor-selectize.git'
 });
 
 Package.onUse(function(api) {
 
-    api.versionsFrom("METEOR@0.9.3");
+  api.versionsFrom("METEOR@0.9.3");
 
-    api.use('jquery', 'client');
+  api.use('jquery', 'client');
 
-    api.addFiles([
-        'selectize/dist/css/selectize.default.css',
-        'selectize/dist/js/standalone/selectize.js'
-    ], ['client']);
+  api.addFiles([
+    'selectize/dist/css/selectize.default.css',
+    'selectize/dist/js/standalone/selectize.js'
+  ], ['client']);
 
 });
