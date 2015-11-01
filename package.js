8837b71c37adf28c48240da031abc98809f00206
Jamie Collinson <jamiecollinson@gmail.com>
2015-11-01 15:36:57 +0000

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "jamiecollinson:selectize-bootstrap",
   summary: "Selectize v0.12.1 is a hybrid of a textbox and <select> element used for tagging, contact lists, and more. This version includes bootstrap 3 styles.",
   version: '0.12.1',
-  git: 'https://github.com/jamiecollinson/meteor-selectize.git'
+  git: 'https://github.com/jamiecollinson/meteor-selectize-bootstrap.git'
 });
 
 Package.onUse(function(api) {
