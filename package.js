22c87799f62d73282b4dc79bc496e74307facc80
Will <wanglian1024@gmail.com>
2022-08-01 00:06:37 -0400

    update version

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: "jamiecollinson:selectize-bootstrap",
-  summary: "Selectize v0.12.1 is a hybrid of a textbox and <select> element used for tagging, contact lists, etc",
-  version: '0.12.1',
-  git: 'https://github.com/jamiecollinson/meteor-selectize-bootstrap.git'
+  summary: "Selectize v0.13.6 is a hybrid of a textbox and <select> element used for tagging, contact lists, etc",
+  version: '0.13.6',
+  git: 'https://github.com/wanglian/meteor-selectize-bootstrap.git'
 });
 
 Package.onUse(function(api) {
